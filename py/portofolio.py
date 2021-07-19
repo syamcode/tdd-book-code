760b4cbb93c5e6faf016c57d8d9b3c8a6310af48
Muhamad Hisyam Anshory <syamcode@gmail.com>
2021-07-19 17:35:38 +0700

    feat: improved error handling for missing exchange rates

--- py/portofolio.py
+++ py/portofolio.py
@@ -7,31 +7,34 @@ class Portofolio:
     def __init__(self):
         self.moneys = []
         self.exchange_rates = {
-            "EUR": {
-                "USD": 1.2,
-                "KRW": 1344,
-            },
-            "USD": {
-                "EUR": 0.82,
-                "KRW": 1100,
-            },
-            "KRW": {
-                "USD": 0.00090,
-                "EUR": 0.00073,
-            },
+            "EUR->USD": 1.2,
+            "EUR->KRW": 1344,
+            "USD->EUR": 0.82,
+            "USD->KRW": 1100,
+            "KRW->USD": 0.00090,
+            "KRW->EUR": 0.00073,
         }
 
     def __convert(self, aMoney, aCurrency):
         if aMoney.currency == aCurrency:
             return aMoney.amount
 
-        return aMoney.amount * self.exchange_rates[aMoney.currency][aCurrency]
+        return aMoney.amount * self.exchange_rates[f"{aMoney.currency}->{aCurrency}"]
 
     def add(self, *moneys):
         self.moneys.extend(moneys)
 
     def evaluate(self, currency):
-        total = functools.reduce(
-            operator.add, map(lambda m: self.__convert(m, currency), self.moneys)
-        )
-        return Money(total, currency)
+        total = 0.0
+        failures = []
+        for m in self.moneys:
+            try:
+                total += self.__convert(m, currency)
+            except KeyError as ke:
+                failures.append(ke)
+
+        if len(failures) == 0:
+            return Money(total, currency)
+        
+        failureMessage = ",".join(f.args[0] for f in failures)
+        raise Exception("Missing exchange rate(s):[" + failureMessage + "]")
--- py/test_money.py
+++ py/test_money.py
@@ -47,6 +47,18 @@ class TestMoney(unittest.TestCase):
         actualValue = portofolio.evaluate("KRW")
         self.assertEqual(expectedValue, actualValue,
                         "%s != %s"%(expectedValue, actualValue))
+
+    def testAdditionWithMultipleMissingExchangeRates(self):
+        oneDollar = Money(1, "USD")
+        oneEuro = Money(1, "EUR")
+        oneWon = Money(1, "KRW")
+        portofolio = Portofolio()
+        portofolio.add(oneDollar, oneEuro, oneWon)
+        with self.assertRaisesRegex(
+            Exception,
+            "Missing exchange rate\(s\):\[USD\->Kalganid,EUR->Kalganid,KRW->Kalganid]"
+        ):
+            portofolio.evaluate("Kalganid")
     
 
 if __name__ == '__main__':
